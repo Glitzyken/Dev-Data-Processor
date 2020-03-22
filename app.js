65ab372a7a72d49519d62b858a494c2d66520b0d
Kenneth Jimmy <kenjimmy17@gmail.com>
2020-03-22 19:42:03 +0100

    format nameInput to lowercase

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ let recipeFile, makeRecipeFile;
 let createButton = document.getElementById("create"),
   nameInput = document.getElementById("name"),
   descInput = document.getElementById("description"),
-  plusIngredient = document.getElementById("plus-ingredient"),
-  plusMethod = document.getElementById("plus-method");
+  noteInput = document.getElementById("note"),
+  ingredientsInput = document.querySelectorAll(".ingredient"),
+  methodsInput = document.querySelectorAll(".method");
 
 ///////////////// MAKE RECIPE DATA
 
@@ -18,8 +19,7 @@ recipeFile = null;
 makeRecipeFile = value => {
   let data = new Blob([value], { type: "text/plain" });
 
-  // If we are replacing a previously generated file we need to
-  // manually revoke the object URL to avoid memory leaks.
+  // If replacing a previously generated file, manually revoke the object URL to avoid memory leaks.
   if (recipeFile !== null) {
     window.URL.revokeObjectURL(recipeFile);
   }
@@ -28,16 +28,6 @@ makeRecipeFile = value => {
 
   return recipeFile;
 };
-
-plusIngredient.addEventListener("click", () => {
-  console.log("Button clicked");
-
-  let html = '<input class="recipe ui" type="text" placeholder="ingredient" />';
-
-  document
-    .getElementById("ingredients")
-    .insertAdjacentElement("beforeend", html);
-});
 
 ///////////////// CREATE RECIPE BUTTON
 
@@ -49,11 +39,29 @@ createButton.addEventListener(
     // Normally, HTML anchor element for download button should have the attribute download set to "name.txt"
     // However, attribute is dynamically set here to reflect the name of each recipe entered as a name input value
     link.setAttribute("download", `${nameInput.value}.txt`);
-    // Prepare and structure the final output for recipeFile. Then, create download link for it.
+
+    // Format and structure the final output value of recipeFile then, create download link for it.
+    name = nameInput.value.toLowerCase();
+    description = descInput.value.toLowerCase();
+
     link.href = makeRecipeFile(
-      `{\n"name": "${nameInput.value}", \n "description": "${descInput.value}"\n},`
+      `{\n"name": "${name}", \n "description": "${description}", \n "note": "${noteInput.value}" \n },`
     );
     link.style.display = "block";
   },
   false
 );
+
+// console.log(typeof name);
+
+//TODO:
+
+// Add Country data
+
+/////// FORMATING DATA VALUE
+// line breaks
+// full stop
+// lowercase
+
+/////// FINISHING POINT
+// Create v1.0
