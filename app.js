d4d9004204346812989f223616db4c9baa0969f5
Kenneth Jimmy <kenjimmy17@gmail.com>
2020-03-22 22:45:11 +0100

    access all 18 nodelist

--- app.js
+++ app.js
@@ -8,8 +8,25 @@ let createButton = document.getElementById("create"),
   nameInput = document.getElementById("name"),
   descInput = document.getElementById("description"),
   noteInput = document.getElementById("note"),
-  ingredientsInput = document.querySelectorAll(".ingredient"),
-  methodsInput = document.querySelectorAll(".method");
+  cntryInput = document.getElementById("country"),
+  ing1 = document.getElementById("ingredient1"),
+  ing2 = document.getElementById("ingredient2"),
+  ing3 = document.getElementById("ingredient3"),
+  ing4 = document.getElementById("ingredient4"),
+  ing5 = document.getElementById("ingredient5"),
+  ing6 = document.getElementById("ingredient6"),
+  ing7 = document.getElementById("ingredient7"),
+  ing8 = document.getElementById("ingredient8"),
+  ing9 = document.getElementById("ingredient9"),
+  ing10 = document.getElementById("ingredient10"),
+  ing11 = document.getElementById("ingredient11"),
+  ing12 = document.getElementById("ingredient12"),
+  ing13 = document.getElementById("ingredient13"),
+  ing14 = document.getElementById("ingredient14"),
+  ing15 = document.getElementById("ingredient15"),
+  ing16 = document.getElementById("ingredient16"),
+  ing17 = document.getElementById("ingredient17"),
+  ing18 = document.getElementById("ingredient18");
 
 ///////////////// MAKE RECIPE DATA
 
@@ -29,14 +46,6 @@ makeRecipeFile = value => {
   return recipeFile;
 };
 
-///////////////// COLLECT DATA FROM INGREDIENT AND METHOD INPUT VALUES
-
-// let ingredientArr = Object.keys(ingredientsInput).forEach(e => {
-//   return ingredientsInput.value;
-// });
-
-let ingredientArr = [...ingredientsInput].map(e => e.value);
-
 ///////////////// CREATE RECIPE BUTTON
 
 createButton.addEventListener(
@@ -47,24 +56,38 @@ createButton.addEventListener(
     // Format and structure the final output value of recipeFile then, create download link for it.
     name = nameInput.value.toLowerCase();
     description = descInput.value.toLowerCase();
+    ingredient1 = ing1.value.toLowerCase();
+    ingredient2 = ing2.value.toLowerCase();
+    ingredient3 = ing3.value.toLowerCase();
+    ingredient4 = ing4.value.toLowerCase();
+    ingredient5 = ing5.value.toLowerCase();
+    ingredient6 = ing6.value.toLowerCase();
+    ingredient7 = ing7.value.toLowerCase();
+    ingredient8 = ing8.value.toLowerCase();
+    ingredient9 = ing9.value.toLowerCase();
+    ingredient10 = ing10.value.toLowerCase();
+    ingredient11 = ing11.value.toLowerCase();
+    ingredient12 = ing12.value.toLowerCase();
+    ingredient13 = ing13.value.toLowerCase();
+    ingredient14 = ing14.value.toLowerCase();
+    ingredient15 = ing15.value.toLowerCase();
+    ingredient16 = ing16.value.toLowerCase();
+    ingredient17 = ing17.value.toLowerCase();
+    ingredient18 = ing18.value.toLowerCase();
+    note = noteInput.value.toLowerCase();
+    country = cntryInput.value.toLowerCase();
 
     // Normally, HTML anchor element for download button should have the attribute download set to "name.txt"
     // However, attribute is dynamically set here to reflect the name of each recipe entered as a name input value
-    link.setAttribute("download", `${nameInput.value}.txt`);
+    link.setAttribute("download", `${name}.txt`);
 
     link.href = makeRecipeFile(
-      `{\n"name": "${name}", \n "description": "${description}", \n "ingredients": ["${[
-        ingredientArr
-      ]}"], \n "note": "${noteInput.value}" \n },`
+      `{\n"name": "${name}", \n "description": "${description}", \n "ingredients": ["${ingredient1}", "${ingredient2}", "${ingredient3}", "${ingredient4}", "${ingredient5}", "${ingredient6}", "${ingredient7}", "${ingredient8}", "${ingredient9}", "${ingredient10}", "${ingredient11}", "${ingredient12}", "${ingredient13}", "${ingredient14}", "${ingredient15}", "${ingredient16}", "${ingredient17}", "${ingredient18}"], \n "note": "${note}", \n "country": "${country}" \n },`
     );
     link.style.display = "block";
   },
   false
 );
-
-console.log(typeof ingredientArr[0]);
-console.log(ingredientArr);
-// console.log(typeof name);
 
 //TODO:
 
